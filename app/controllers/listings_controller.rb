610fe92cb5054f17f4ea4402c9e4792998081483
sjwconway <sjwconway@hotmail.com>
2019-04-18 13:04:18 +1000

    created form partial and edit views

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,11 +1,19 @@
 class ListingsController < ApplicationController 
     before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    before_action :set_breeds_and_sexes, only: [:new, :edit]
     
     def create
         #create new listing
-        
         @listing = Listing.create(listing_params)
-        byebug
+        
+
+        if @listing.errors.any?
+            set_breeds_and_sexes
+            render "new"
+
+        else
+            redirect_to listing_path
+        end
         
     end
 
@@ -34,14 +42,14 @@ class ListingsController < ApplicationController
     def new
         #shows form for creating a new listing
         @listing = Listing.new
-        @breeds = Breed.all
-        @sexes = Listing.sexes.keys
+        
     end
 
     def show
         #shows a single listig
         
     end
+    
     
     private
 
@@ -52,6 +60,15 @@ class ListingsController < ApplicationController
 
     def listing_params
         params.require(:listing).permit(:title, :description, :breed_id, :sex, :price, :deposit, :date_of_birth, :diet)
+
     end
+
+    def set_breeds_and_sexes
+        @breeds = Breed.all
+        @sexes = Listing.sexes.keys
+        p @breeds
+    end
+
+    
     
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ApplicationRecord
   belongs_to :breed
   enum sex: { female: 0, male: 1}
+  validates :title, :breed_id, :sex, :price, :date_of_birth, :description, presence: true
 end
